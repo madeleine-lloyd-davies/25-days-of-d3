1663d55cb51e4371ad897a1694722a6710ea0db1
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2022-12-28 17:07:11 -0600

    completed day 3

--- index.js
+++ index.js
@@ -144,3 +144,45 @@ function day2pt2() {
   return svgHTML;
 }
 day2pt2();
+
+function day3() {
+  const data = [
+    { name: 'a', value: 1 },
+    { name: 'b', value: 2 },
+    { name: 'c', value: 3 },
+    { name: 'd', value: 5 },
+    { name: 'e', value: 8 },
+  ];
+  const svg = d3.select('#day-3-svg');
+
+  const circles = svg.selectAll('circle').data(data);
+
+  circles
+    .enter()
+    .append('circle')
+    // set the circles' radius
+    .attr('r', (d) => d.value * 10)
+    // set the circles' x position
+    .attr('cx', (d) => d.value * 10)
+    // set the circles' y position
+    .attr('cy', 80)
+    .attr('fill', 'transparent')
+    .attr('stroke', 'steelblue');
+
+  const text = svg.selectAll('text').data(data);
+
+  text
+    .enter()
+    .append('text')
+    .attr('x', (d) => d.value * 10 * 2)
+    .attr('y', 80)
+    // This is a "magic number" in svg. It makes sure the text is vertically centered
+    .attr('dy', '0.35em')
+    // set from which direction the text expands
+    .attr('text-anchor', 'end')
+    // set the actual text
+    .text((d) => d.name);
+
+  return svg;
+}
+day3();
